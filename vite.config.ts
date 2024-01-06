dbbebc757c22e309e66bd066180c0a42c07cc609
神代綺凛 <i@loli.best>
2024-01-06 19:22:50 +0800

    fix: vercel site

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import { rename } from 'fs/promises';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vercel from 'vite-plugin-vercel';
 import svgLoader from 'vite-svg-loader';
 import { startApiServer } from './server/utils';
+import type { ViteVercelConfig } from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -14,5 +16,11 @@ export default defineConfig(({ command }) => {
         '/api': 'http://127.0.0.1:3000',
       },
     },
+    vercel: {
+      prerender: async () => {
+        await rename('dist', '.vercel/output/static');
+        return {};
+      },
+    } as ViteVercelConfig,
   };
 });
